e4f61c5b1ca032a3c915a20b2550fe4fbe389d91
edmundreinhardt <edmund.reinhardt@ca.ibm.com>
2025-09-24 17:25:21 -0400

    top level project and Rules.mk
    Signed-off-by: edmundreinhardt <edmund.reinhardt@ca.ibm.com>

--- refactor/country-management/qsqlsrc/Rules.mk
+++
@@ -1,9 +0,0 @@
-# Rules.mk for qsqlsrc directory
-# This file defines dependencies for building FILE objects from table source files
-
-COUNTRY.FILE: COUNTRY.table
-COUNTR1.FILE: COUNTR1.index COUNTRY.FILE
-
-# Made with Better Object Builder
-
-# Made with Bob
