dc60ca0191d085011dc2f289b87553abb5c66b36
edmundreinhardt <edmund.reinhardt@ca.ibm.com>
2025-09-24 15:55:23 -0400

    Full Rules.mk generated by Source Orbit
    Signed-off-by: edmundreinhardt <edmund.reinhardt@ca.ibm.com>

---
+++ refactor/country-management/Rules.mk
@@ -0,0 +1,1 @@
+SUBDIRS = qsqlsrc qddssrc qrpglesrc
--- refactor/country-management/qsqlsrc/Rules.mk
+++ refactor/country-management/qsqlsrc/Rules.mk
@@ -2,7 +2,7 @@
 # This file defines dependencies for building FILE objects from table source files
 
 COUNTRY.FILE: COUNTRY.table
-COUNTR1.FILE: COUNTR1.index
+COUNTR1.FILE: COUNTR1.index COUNTRY.FILE
 
 # Made with Better Object Builder
 
