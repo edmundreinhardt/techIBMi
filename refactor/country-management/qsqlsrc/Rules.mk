3ad7f67ba8ac65a7531e60104eb69ab07befd1c8
edmundreinhardt <edmund.reinhardt@ca.ibm.com>
2025-09-24 15:47:50 -0400

    - Converted DDS database source to DDL format - Created COUNTRY.table with proper field definitions and primary key - Created COUNTR1.index for the country name index - Generated Rules.mk for the database layer with proper dependencies - All files are formatted for Better Object Builder compatibility
    Signed-off-by: edmundreinhardt <edmund.reinhardt@ca.ibm.com>

---
+++ refactor/country-management/qsqlsrc/Rules.mk
@@ -0,0 +1,9 @@
+# Rules.mk for qsqlsrc directory
+# This file defines dependencies for building FILE objects from table source files
+
+COUNTRY.FILE: COUNTRY.table
+COUNTR1.FILE: COUNTR1.index
+
+# Made with Better Object Builder
+
+# Made with Bob
